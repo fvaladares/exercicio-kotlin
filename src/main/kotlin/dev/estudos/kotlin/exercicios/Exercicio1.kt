c5575491947f70b0a8f58b99bc4ba12e8d1662cf
Fabrício Valadares <fabricio.valadares@gmail.com>
2021-01-30 13:33:01 -0300

    Exercicio1.kt Refactored, now all the actions are inside the class Point.

--- src/main/kotlin/dev/estudos/kotlin/exercicios/Exercicio1.kt
+++ src/main/kotlin/dev/estudos/kotlin/exercicios/Exercicio1.kt
@@ -18,40 +18,43 @@ fun main() {
 
     println(message)
 
-    val p1: Point = newPoint()
-    val p2: Point = newPoint()
+    val p1 = Point.create()
+    val p2 = Point.create()
 
 
-    printPoint(p1, 1)
-    printPoint(p2, 2)
+    p1.printPoint(1)
+    p2.printPoint(2)
+
     println("\nThe distance between P1 and P2 is ${p1.distanceTo(p2)}")
-
     println("Program finished. \nThanks!!")
 }
 
 
-fun printPoint(p: Point, id: Int) {
-    println()
-    println("Point $id \n Coordinates: x: ${p.x}, y: ${p.y}")
-}
-
-fun newPoint(): Point {
-    println()
-    print("Please, inform the X coordinate for the new point: ")
-    val x = readInt()
-    println()
-    print("Please, inform the Y coordinate for the new point: ")
-    val y = readInt()
-
-    return Point(x, y)
-}
-
-class Point(val x: Int, val y: Int) {
+class Point(private val x: Int, private val y: Int) {
     fun distanceTo(p2: Point): Double {
         return sqrt(
             ((p2.x - this.x).toDouble().pow(2)) +
                     ((p2.y - this.y).toDouble().pow(2))
         )
+    }
+
+
+    fun printPoint(id: Int = 0) {
+        println()
+        println("Point $id \n Coordinates: x: ${this.x}, y: ${this.y}")
+    }
+
+    companion object {
+        fun create(): Point {
+            println()
+            print("Please, inform the X coordinate for the new point: ")
+            val x = readInt()
+            println()
+            print("Please, inform the Y coordinate for the new point: ")
+            val y = readInt()
+
+            return Point(x, y)
+        }
     }
 }
 
