03c7882d5ca464250554ef789ddabaa039e947e2
Fabrício Valadares <fabricio.valadares@gmail.com>
2021-02-01 20:48:54 -0300

    Object Calc.kt created:    *We have two functions:    ** add the sums two numbers and returns a double    ** operation, that executes some operation.

--- src/test/kotlin/dev/estudos/kotlin/exercicios/calculadora/CalcTest.kt
+++ src/test/kotlin/dev/estudos/kotlin/exercicios/calculadora/CalcTest.kt
@@ -7,14 +7,26 @@ import org.junit.jupiter.api.Test
 internal class CalcTest {
 
     @Test
-    @DisplayName("Add 4 + 2, expecting result 6")
+    @DisplayName("Add 25 + 99 = 124")
     fun operationCase1() {
-        Assertions.assertEquals(6.0, Calc.operation(Calc.add(4.0, 2.0)))
+        Assertions.assertEquals(124.0, Calc.operation(Calc.add(25.0, 99.0)))
     }
 
     @Test
-    @DisplayName("Add -4 + 2, expecting result -4")
+    @DisplayName("Add -15 + -8 = -23")
     fun operationCase2() {
-        Assertions.assertEquals(-2.0, Calc.operation(Calc.add(-4.0, 2.0)))
+        Assertions.assertEquals(-23.0, Calc.operation(Calc.add(-15.0, -8.0)))
+    }
+
+    @Test
+    @DisplayName("Add 1.5 + 2.2 = 3.7")
+    fun operationCase3() {
+        Assertions.assertEquals(3.7, Calc.operation(Calc.add(1.5, 2.2)))
+    }
+
+    @Test
+    @DisplayName("Add -10.1 + 5 = -5.1")
+    fun operationCase4() {
+        Assertions.assertEquals(-5.1, Calc.operation(Calc.add(-10.1, 5.0)))
     }
 }
