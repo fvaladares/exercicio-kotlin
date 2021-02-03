1fa3d54701b1f98dad0008caa567ea10b6c5ffc3
Fabrício Valadares <fabricio.valadares@gmail.com>
2021-02-03 20:56:15 -0300

    Added PessoaRepositoryTest.kt

---
+++ src/test/kotlin/dev/estudos/kotlin/exercicios/pessoas/repository/PessoaRepositoryTest.kt
@@ -0,0 +1,38 @@
+package dev.estudos.kotlin.exercicios.pessoas.repository
+
+import org.junit.jupiter.api.Test
+
+internal class PessoaRepositoryTest {
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun getAll() {
+    }
+
+    @Test
+    fun getById() {
+    }
+
+    @Test
+    fun findByName() {
+    }
+
+    @Test
+    fun findByCPF() {
+    }
+
+    @Test
+    fun findBy() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
