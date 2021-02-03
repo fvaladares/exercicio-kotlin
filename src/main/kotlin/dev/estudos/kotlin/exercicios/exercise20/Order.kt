b5b99c05ea9b3336fa68ed8c6db6ff5e1ba0252e
Fabrício Valadares <fabricio.valadares@gmail.com>
2021-02-03 11:54:40 -0300

    Working at exercise 20 of the list: #FinalCommit Add classes:    Order.kt    OrderTest.kt    Product.kt    ProductTest.kt

--- src/main/kotlin/dev/estudos/kotlin/exercicios/exercise20/Order.kt
+++ src/main/kotlin/dev/estudos/kotlin/exercicios/exercise20/Order.kt
@@ -1,19 +1,51 @@
 package dev.estudos.kotlin.exercicios.exercise20
 
+
+/**
+ * This class represents an Order made by a client
+ * It keep the products in a List
+ * You can use the method add to add one Product
+ * Or you ca use the method addAll to add several Products
+ */
 class Order(val orderNumber: Int) {
     private var products = mutableListOf<Product>()
 
+    /**
+     * @param newProduct
+     * @return an updated Order
+     */
     fun add(newProduct: Product): Order {
         products.add(newProduct)
         return this
     }
 
+    /**
+     * @param newProducts
+     * @return an updated Order
+     */
     fun addAll(newProducts: List<Product>): Order {
         products.addAll(newProducts)
         return this
     }
 
-    fun finalPrice(cod: Int, amount: Int) {
-
+    /** The calculation is done with BigDecimal.
+     * @param cod, amount
+     * @return Double
+     */
+    fun finalPrice(cod: Int, amount: Int): Double {
+        val test = products
+            .filter { it.code == cod }
+            .map { it.unitPrice }.reduce { acc, d -> acc + d }
+        /**
+         * Log for debug purpose
+         */
+        println(
+            """
+            Product returned    =   $test
+            Amount              =   $amount
+            Total               =   ${amount * test}
+        """.trimIndent()
+        )
+        return ((test.toBigDecimal() * amount.toBigDecimal()).toDouble())
     }
 }
--- src/test/kotlin/dev/estudos/kotlin/exercicios/exercise20/OrderTest.kt
+++ src/test/kotlin/dev/estudos/kotlin/exercicios/exercise20/OrderTest.kt
@@ -2,43 +2,25 @@ package dev.estudos.kotlin.exercicios.exercise20
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class OrderTest {
+    private lateinit var order: Order
 
     @Test
     fun add() {
         val product = Product(1001, 2.50)
         val order = Order(1)
-
-
         assertTrue(order.add(product) === order)
-
-//        val p1: Product = product.copy()
-//
-//        if (product == p1) {
-//            println("Test passed ${product.code}")
-//            println("""
-//                Product:
-//                   code: ${product.code}
-//                   price $${product.unitPrice}
-//            """.trimIndent())
-//
-//            println("""
-//                P1:
-//                   code: ${p1.code}
-//                   price $${p1.unitPrice}
-//            """.trimIndent())
-//
-//        } else {
-//            println("Test Failed")
-//        }
     }
 
     @Test
     fun addAll() {
         val product = Product(1001, 2.50)
-        val order = Order(1)
+        this.order = Order(1)
 
         with(order) {
             addAll(
@@ -57,5 +39,32 @@ internal class OrderTest {
     fun getOrderNumber() {
         val orderNumber = 101
         assertEquals(orderNumber, Order(orderNumber).orderNumber)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1001,  1,      5.32",
+        "1324,  10,     64.50",
+        "6548,   3,     7.11",
+        "987,    8,     42.56",
+        "7623,   10,    64.50"
+    )
+    fun finalPrice(cod: Int, amount: Int, expected: Double) {
+        assertEquals(expected, order.finalPrice(cod, amount))
+    }
+
+    // Create an object with the values gave by US.
+    @BeforeEach
+    fun init() {
+        println("Starting tests...")
+        order = Order(1)
+        val products = listOf(
+            Product(1001, 5.32),
+            Product(1324, 6.45),
+            Product(6548, 2.37),
+            Product(987, 5.32),
+            Product(7623, 6.45)
+        )
+        order.addAll(products)
     }
 }
