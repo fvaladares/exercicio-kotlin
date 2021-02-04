28880a203f9a66a3a07d7326024c7c8aba01521c
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-02-04 20:35:03 -0300

    PessoaRepositoryTest.kt refactored
        (BeforeEach e AfterEach annotation inserted)

--- src/test/kotlin/dev/estudos/kotlin/exercicios/pessoas/repository/PessoaRepositoryTest.kt
+++ src/test/kotlin/dev/estudos/kotlin/exercicios/pessoas/repository/PessoaRepositoryTest.kt
@@ -2,79 +2,92 @@ package dev.estudos.kotlin.exercicios.pessoas.repository
 
 import dev.estudos.kotlin.exercicios.pessoas.model.Address
 import dev.estudos.kotlin.exercicios.pessoas.model.Pessoa
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 internal class PessoaRepositoryTest {
 
-    private val pessoa: Pessoa = Pessoa.create(
-        name = "Test User",
-        birthday = "16/11/1982",
-        phone = "31 993576193",
-        cpf = "11111111111",
-        rg = "mg00111222",
-        address = Address(
-            "Rua Dama da Noite",
-            "200",
-            "Morada dos hibiscos",
-            "Pedro Leopoldo",
-            "MG"
-        ),
-    )
-    private val pessoa2 = Pessoa.create(
-        name = "Test User",
-        birthday = "16/11/1982",
-        phone = "31 993576193",
-        cpf = "11111111112",
-        rg = "mg00111222",
-        address = Address(
-            "Rua Dama da Noite",
-            "200",
-            "Morada dos hibiscos",
-            "Pedro Leopoldo",
-            "MG"
-        ),
-    )
-    private val repository = PessoaRepository()
+    //Mock data:
 
-//@BeforeEach
-//    fun init() {
-//        pessoa = Pessoa.create(
-//            name = "Test User",
-//            birthday = "16/11/1982",
-//            phone = "31 993576193",
-//            cpf = "11111111111",
-//            rg = "mg00111222",
-//            address = Address(
-//                "Rua Dama da Noite",
-//                "200",
-//                "Morada dos hibiscos",
-//                "Pedro Leopoldo",
-//                "MG"
-//            ),
-//        )
-//
-//        pessoa2 = Pessoa.create(
-//            name = "Test User",
-//            birthday = "16/11/1982",
-//            phone = "31 993576193",
-//            cpf = "11111111112",
-//            rg = "mg00111222",
-//            address = Address(
-//                "Rua Dama da Noite",
-//                "200",
-//                "Morada dos hibiscos",
-//                "Pedro Leopoldo",
-//                "MG"
-//            ),
-//        )
-//
-//        repository = PessoaRepository()
-//    }
-//@AfterEach
-//    fun cleanUp() {
-//        repository = PessoaRepository()
-//    }
+    val listOfPessoa by lazy {
+        mutableListOf<Pessoa>()
+    }
+
+    private lateinit var pessoa: Pessoa
+    private lateinit var pessoa2: Pessoa
+
+//    private val pessoa: Pessoa = Pessoa.create(
+//        name = "Test User",
+//        birthday = "16/11/1982",
+//        phone = "31 993576193",
+//        cpf = "11111111111",
+//        rg = "mg00111222",
+//        address = Address(
+//            "Rua Dama da Noite",
+//            "200",
+//            "Morada dos hibiscos",
+//            "Pedro Leopoldo",
+//            "MG"
+//        ),
+//    )
+
+    //    private val pessoa2 = Pessoa.create(
+//        name = "Test User",
+//        birthday = "16/11/1982",
+//        phone = "31 993576193",
+//        cpf = "11111111112",
+//        rg = "mg00111222",
+//        address = Address(
+//            "Rua Dama da Noite",
+//            "200",
+//            "Morada dos hibiscos",
+//            "Pedro Leopoldo",
+//            "MG"
+//        ),
+//    )
+    private lateinit var repository: PessoaRepository
+
+    @BeforeEach
+    fun init() {
+        pessoa = Pessoa.create(
+            name = "Test User",
+            birthday = "16/11/1982",
+            phone = "31 993576193",
+            cpf = "11111111111",
+            rg = "mg00111222",
+            address = Address(
+                "Rua Dama da Noite",
+                "200",
+                "Morada dos hibiscos",
+                "Pedro Leopoldo",
+                "MG"
+            ),
+        )
+
+        pessoa2 = Pessoa.create(
+            name = "Test User",
+            birthday = "16/11/1982",
+            phone = "31 993576193",
+            cpf = "11111111112",
+            rg = "mg00111222",
+            address = Address(
+                "Rua Dama da Noite",
+                "200",
+                "Morada dos hibiscos",
+                "Pedro Leopoldo",
+                "MG"
+            ),
+        )
+
+        repository = PessoaRepository()
+    }
+
+    @AfterEach
+    private fun cleanUp() {
+        repository = PessoaRepository()
+    }
 
 
     @Test
@@ -107,8 +120,8 @@ internal class PessoaRepositoryTest {
     fun findByName() {
     }
 
-    @Test
-    fun findByCPF(cpf: String): List<Pessoa> {
+
+    private fun findByCPF(cpf: String): List<Pessoa> {
         return repository.findByCPF(cpf)
     }
 
