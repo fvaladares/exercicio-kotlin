7d493cbff832f23400e2b169bc62f0fa4d9b73ce
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-02-06 11:46:04 -0300

    Exercise3Test.kt refactored, test pass.

--- src/test/kotlin/dev/estudos/kotlin/exercicios/Exercise3Test.kt
+++ src/test/kotlin/dev/estudos/kotlin/exercicios/Exercise3Test.kt
@@ -2,13 +2,20 @@ package dev.estudos.kotlin.exercicios
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 internal class Exercise3Test {
 
     @Test
+//    @Disabled("O número de dias pode falhar pois o ")
     fun giveNumberOfDaysFromAnStringDateddMMyyyy() {
         val e3Date = Exercise3()
 
-        assertEquals(13957, e3Date.giveNumberOfDaysFromAnStringDateddMMyyyy("16/11/1982"))
+        val date = "16/11/1982"
+
+        assertEquals(ChronoUnit.DAYS.between(LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+            LocalDate.now()), e3Date.giveNumberOfDaysFromAnStringDateddMMyyyy(date))
     }
 }
